5ab5e46c603e594e29e483f136e53bb86cc683d7
labcabrera <lab.cabrera@gmail.com>
2025-07-12 23:26:52 +0200

    Updated configuration.

--- src/constants/environment.js
+++ src/constants/environment.js
@@ -1,6 +1,10 @@
-const API_NPC_URL= process.env.API_NPC_URL || "http://localhost:3004/v1" ;
-const API_TACTICAL_URL= process.env.API_STRATEGIC_URL || "http://localhost:3003/v1" ;
-const API_STRATEGIC_URL = process.env.API_STRATEGIC_URL || "http://localhost:3002/v1" ;
+// const API_NPC_URL= process.env.API_NPC_URL || "http://localhost:3004/v1" ;
+// const API_TACTICAL_URL= process.env.API_STRATEGIC_URL || "http://localhost:3003/v1" ;
+// const API_STRATEGIC_URL = process.env.API_STRATEGIC_URL || "http://localhost:3002/v1" ;
+
+const API_NPC_URL= process.env.API_NPC_URL || "http://api-npc.rmu.local/v1" ;
+const API_TACTICAL_URL= process.env.API_STRATEGIC_URL || "http://api-tactical.rmu.local/v1" ;
+const API_STRATEGIC_URL = process.env.API_STRATEGIC_URL || "http://api-strategic.rmu.local/v1" ;
 
 export {
     API_NPC_URL,
