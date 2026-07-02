371562018c09dd8ec079638c112e0e2892281943
labcabrera <lab.cabrera@gmail.com>
2026-07-02 14:39:08 +0200

    Use RmuDialog.

--- src/modules/npcs/view/skills/AddSkillDialog.tsx
+++ src/modules/npcs/view/skills/AddSkillDialog.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from '@mui/material';
-import { AddNpcSkill, NumericInput, SkillSelector } from '@labcabrera-rmu/rmu-react-shared-lib';
+import { Button, Grid } from '@mui/material';
+import { AddNpcSkill, NumericInput, RmuDialog, SkillSelector } from '@labcabrera-rmu/rmu-react-shared-lib';
 import { useError } from '../../../../ErrorContext';
 
 export default function AddSkillDialog({
@@ -29,49 +29,50 @@ export default function AddSkillDialog({
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="xl" fullWidth>
-      <DialogTitle>{t('add-skill')}</DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2} sx={{ mt: 1 }}>
-          <Grid size={12}>
-            <SkillSelector
-              onSkillChange={(e) => setFormData({ ...formData, skillId: e! })}
-              onSpecializationChange={(e) => setFormData({ ...formData, specialization: e! })}
-              onError={(e) => showError(e)}
-            />
-          </Grid>
-          <Grid size={12}>
-            {formData.skillId && (
-              <>
-                <Grid container spacing={2}>
-                  <Grid size={12}>
-                    <NumericInput
-                      label={t('ranks')}
-                      value={formData.ranks}
-                      onChange={(e) => setFormData({ ...formData, ranks: e || 0 })}
-                      integer
-                    />
-                  </Grid>
-                  <Grid size={12}>
-                    <NumericInput
-                      label={t('bonus')}
-                      value={formData.bonus}
-                      onChange={(e) => setFormData({ ...formData, bonus: e || 0 })}
-                      integer
-                    />
-                  </Grid>
-                </Grid>
-              </>
-            )}
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>{t('close')}</Button>
+    <RmuDialog
+      title={t('add-skill')}
+      open={open}
+      onClose={handleClose}
+      maxWidth="xl"
+      buttons={
         <Button onClick={handleAdd} variant="contained" disabled={!formData.skillId}>
           {t('add')}
         </Button>
-      </DialogActions>
-    </Dialog>
+      }
+    >
+      <Grid container spacing={2} sx={{ mt: 1 }}>
+        <Grid size={12}>
+          <SkillSelector
+            onSkillChange={(e) => setFormData({ ...formData, skillId: e! })}
+            onSpecializationChange={(e) => setFormData({ ...formData, specialization: e! })}
+            onError={(e) => showError(e)}
+          />
+        </Grid>
+        <Grid size={12}>
+          {formData.skillId && (
+            <>
+              <Grid container spacing={2}>
+                <Grid size={12}>
+                  <NumericInput
+                    label={t('ranks')}
+                    value={formData.ranks}
+                    onChange={(e) => setFormData({ ...formData, ranks: e || 0 })}
+                    integer
+                  />
+                </Grid>
+                <Grid size={12}>
+                  <NumericInput
+                    label={t('bonus')}
+                    value={formData.bonus}
+                    onChange={(e) => setFormData({ ...formData, bonus: e || 0 })}
+                    integer
+                  />
+                </Grid>
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </Grid>
+    </RmuDialog>
   );
 }
